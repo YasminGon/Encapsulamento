d46967594ad3fce2dc729b91d89a4ff0b18d1968
YasminGon <yasmingon08@hotmail.com>
2023-04-04 14:21:18 +0000

    gravando

---
+++ Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+    public static void main(String[] args) {
+        
+        Conta conta=new Conta();
+        conta.setNumero(42);
+        
+        //conta.nome="Joao";
+        //conta.salario=10000;
+
+        //System.out.println(conta.nome);
+       //System.out.println(conta.salario);
+       System.out.println(conta.getNumero());
+        
+  
+    }
+    
+}
